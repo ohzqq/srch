97a02f57447ec5450824dd4e97feb7dfdfcf9ec8
ohzqq <iamchurkey@gmail.com>
2024-01-09 11:45:53 -0500

    remove config

--- config.go
+++
@@ -1,9 +0,0 @@
-package srch
-
-type Config struct {
-	Fields      []*Field `json:"fields"`
-	Query       Query    `json:"filters"`
-	Identifier  string   `json:"identifier"`
-	interactive bool
-	fuzzy       bool
-}
