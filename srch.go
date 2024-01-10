8ac553c27b167b6d379d867642e8d501f4bebc80
ohzqq <iamchurkey@gmail.com>
2024-01-10 15:30:50 -0500

    remove query

--- srch.go
+++ srch.go
@@ -1,6 +1,7 @@
 package srch
 
 import (
+	"net/url"
 	"strings"
 
 	"github.com/samber/lo"
@@ -46,4 +47,42 @@ func collectResults(d []map[string]any, ids []int) []map[string]any {
 		return data
 	}
 	return d
+}
+
+// ParseValues takes an interface{} and returns a url.Values.
+func ParseValues(f any) (map[string][]string, error) {
+	filters := make(map[string][]string)
+	var err error
+	switch val := f.(type) {
+	case url.Values:
+		return val, nil
+	case []byte:
+		return ParseQueryBytes(val)
+	case string:
+		return ParseQueryString(val)
+	default:
+		filters, err = cast.ToStringMapStringSliceE(val)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return filters, nil
+}
+
+// ParseQueryString parses an encoded filter string.
+func ParseQueryString(val string) (map[string][]string, error) {
+	q, err := url.ParseQuery(val)
+	if err != nil {
+		return nil, err
+	}
+	return q, err
+}
+
+// ParseQueryBytes parses a byte slice to url.Values.
+func ParseQueryBytes(val []byte) (map[string][]string, error) {
+	filters, err := cast.ToStringMapStringSliceE(string(val))
+	if err != nil {
+		return nil, err
+	}
+	return filters, err
 }
