6c0e7a3c3aa06ac68db1d0ee2c49e6b42dccd63f
ohzqq <iamchurkey@gmail.com>
2024-01-24 13:21:12 -0500

    text

--- field/field.go
+++ field/field.go
@@ -1,4 +1,4 @@
-package srch
+package field
 
 import (
 	"encoding/json"
--- kw/keyword.go
+++ kw/keyword.go
@@ -1,4 +1,25 @@
 package kw
 
+import "github.com/spf13/cast"
+
 type Field struct {
+}
+
+type Token struct {
+}
+
+func KeywordAnalyzer(val any) []*Token {
+	var tokens []string
+	switch v := val.(type) {
+	case string:
+		tokens = append(tokens, v)
+	default:
+		tokens = cast.ToStringSlice(v)
+	}
+	items := make([]*Token, len(tokens))
+	for i, token := range tokens {
+		items[i] = NewToken(token)
+		items[i].Value = normalizeText(token)
+	}
+	return items
 }
