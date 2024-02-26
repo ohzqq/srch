6b531002bd2544156e182c4e749a7d3fc29360f2
ohzqq <iamchurkey@gmail.com>
2024-02-26 11:18:24 -0500

    ok

--- bleve_test.go
+++ bleve_test.go
@@ -1,10 +1,15 @@
 package srch
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/blevesearch/bleve/v2"
+	"github.com/spf13/cast"
 )
 
 //func TestDocMapping(t *testing.T) {
@@ -16,18 +21,93 @@ import (
 //  println(string(d))
 //}
 
+const blevePath = `testdata/poot`
+
 func TestNewBleveIndex(t *testing.T) {
-	idxMap := bleve.NewIndexMapping()
-	idxMap.DefaultAnalyzer = StandardAnalyzer
+	idx, err := NewTextIndex(FTPath(blevePath))
+	//idx, err := NewTextIndex(MemOnly)
+	if err != nil {
+		//t.Fatal(err)
+		t.Skipf("%v\n", err)
+	}
 
-	idx, err := bleve.New("testdata/poot", idxMap)
+	d, err := idx.DocCount()
+	if err != nil {
+		t.Error(err)
+	}
+	println(d)
+}
+
+func TestBatchIndex(t *testing.T) {
+	batchSize := 1000
+
+	idx, err := bleve.Open(blevePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer idx.Close()
+
+	file, err := os.Open("testdata/audiobks.json")
 	if err != nil {
 		t.Error(err)
 	}
 
-	d, err := json.Marshal(idx)
+	i := 0
+	batch := idx.NewBatch()
+
+	r := bufio.NewReader(file)
+
+	for {
+		if i%batchSize == 0 {
+			fmt.Printf("Indexing batch (%d docs)...\n", i)
+			err := idx.Batch(batch)
+			if err != nil {
+				t.Error(err)
+			}
+			batch = idx.NewBatch()
+		}
+
+		b, _ := r.ReadBytes('\n')
+		if len(b) == 0 {
+			break
+		}
+
+		var doc interface{}
+		doc = b
+		var err error
+		err = json.Unmarshal(b, &doc)
+		if err != nil {
+			t.Errorf("error parsing JSON: %v", err)
+		}
+
+		book := cast.ToStringMap(doc)
+
+		docID := cast.ToString(book["id"])
+		//docID := cast.ToString(i)
+		err = batch.Index(docID, book)
+		if err != nil {
+			t.Error(err)
+		}
+		i++
+	}
+
+	err = file.Close()
 	if err != nil {
 		t.Error(err)
 	}
-	println(string(d))
+
+}
+
+func cleanIdx() {
+	idxMeta := filepath.Join(blevePath, "index_meta.json")
+	err := os.Remove(idxMeta)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	idxStore := filepath.Join(blevePath, "store")
+	err = os.RemoveAll(idxStore)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }
--- facet.go
+++ facet.go
@@ -1,5 +1,37 @@
 package srch
 
+import "github.com/ohzqq/srch/txt"
+
 type Facet struct {
 	*Field
+}
+
+func NewFacet(attr string) *Facet {
+	f := NewField(attr)
+	f.SetAnalyzer(txt.Keyword())
+
+	return &Facet{
+		Field: f,
+	}
+}
+
+func NewFacets(attrs []string) map[string]*Facet {
+	fields := make(map[string]*Facet)
+	for _, attr := range attrs {
+		fields[attr] = NewFacet(attr)
+	}
+
+	return fields
+}
+
+func CalculateFacets(data []map[string]any, fields []string) map[string]*Facet {
+	facets := NewFacets(fields)
+	for id, d := range data {
+		for attr, facet := range facets {
+			if val, ok := d[attr]; ok {
+				facet.Add(val, []int{id})
+			}
+		}
+	}
+	return facets
 }
