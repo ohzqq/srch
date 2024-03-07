d49b29b6d644ff5d03b165569c7711ec841920db
ohzqq <iamchurkey@gmail.com>
2024-03-07 12:49:30 -0500

    fuzzy

--- search.go
+++ search.go
@@ -1,7 +1,15 @@
 package srch
 
-import "github.com/RoaringBitmap/roaring"
+import (
+	"github.com/RoaringBitmap/roaring"
+)
+
+type Indexer interface {
+	New(settings string) (*Index, error)
+	Open(settings string) (*Index, error)
+	Index(uid string, data ...map[string]any) error
+}
 
 type Searcher interface {
-	Search(string, ...*roaring.Bitmap) (*Response, error)
+	Search(query string) (*roaring.Bitmap, error)
 }
