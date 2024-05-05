f7100bd61fbb8e1d000bd5b8f30072494065533c
ohzqq <iamchurkey@gmail.com>
2024-05-05 10:07:07 -0400

    passing

--- idx/idx_test.go
+++ idx/idx_test.go
@@ -22,6 +22,18 @@ func TestNewDB(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	i := 0
+
+	err = checkAttrs(param.SrchAttr.String(), idx.Params.SrchAttr, paramTests[i].want.SrchAttr)
+	if err != nil {
+		t.Errorf("\nparams: %v\ntest num %v: %v\n", paramTests[i].query, i, err)
+	}
+
+	err = checkAttrs(param.FacetAttr.String(), idx.Params.FacetAttr, paramTests[i].want.FacetAttr)
+	if err != nil {
+		t.Errorf("\nparams: %v\ntest num %v: %v\n", paramTests[i].query, i, err)
+	}
 }
 
 func TestOpenIdx(t *testing.T) {
@@ -34,6 +46,7 @@ func TestOpenIdx(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
+
 		err = checkAttrs(param.SrchAttr.String(), idx.Params.SrchAttr, test.want.SrchAttr)
 		if err != nil {
 			t.Errorf("\nparams: %v\ntest num %v: %v\n", test.query, i, err)
