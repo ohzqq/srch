0ea495327ea0b494fffed03a1fbff3af8dfeda70
ohzqq <iamchurkey@gmail.com>
2024-05-05 09:18:55 -0400

    test utils

--- idx/idx.go
+++ idx/idx.go
@@ -1,20 +1,34 @@
 package idx
 
 import (
+	"fmt"
+
 	"github.com/ohzqq/srch/db"
+	"github.com/ohzqq/srch/doc"
 	"github.com/ohzqq/srch/param"
 )
 
 type Idx struct {
-	DB     *db.DB
+	*db.DB
 	Params *param.Params
 }
 
 func New() *Idx {
+	m := doc.DefaultMapping()
+	db, _ := db.New(m)
 	return &Idx{
 		Params: param.New(),
+		DB:     db,
 	}
 }
 
 func Open(settings string) (*Idx, error) {
+	idx := New()
+	var err error
+	idx.Params, err = param.Parse(settings)
+	if err != nil {
+		return nil, fmt.Errorf("new index param parsing err: %w\n", err)
+	}
+
+	return idx, nil
 }
--- idx/idx_test.go
+++ idx/idx_test.go
@@ -1,1 +1,30 @@
 package idx
+
+import "testing"
+
+const (
+	facetParamStr   = `facets=tags,authors,series,narrators`
+	facetParamSlice = `facets=tags&facets=authors&facets=series&facets=narrators`
+	srchAttrParam   = "searchableAttributes=title"
+	queryParam      = `query=fish`
+	sortParam       = `sortBy=title&order=desc`
+	filterParam     = `facetFilters=["authors:amy lane", ["tags:romance", "tags:-dnr"]]`
+	uidParam        = `uid=id`
+)
+
+func TestNewDB(t *testing.T) {
+	idx := New()
+	if idx.Name != "index" {
+		t.Error("idx.Name isn't index")
+	}
+}
+
+func TestOpenIdx(t *testing.T) {
+	idx, err := Open(srchAttrParam)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if idx.Name != "index" {
+		t.Error("idx.Name isn't index")
+	}
+}
