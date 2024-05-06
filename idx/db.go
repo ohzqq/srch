99a5d80d617bd64898ec431b5bfb21c4d2acfe14
ohzqq <iamchurkey@gmail.com>
2024-05-06 07:52:52 -0400

    opt

---
+++ idx/db.go
@@ -0,0 +1,124 @@
+package idx
+
+import (
+	"github.com/ohzqq/hare"
+	"github.com/ohzqq/hare/datastores/disk"
+	"github.com/ohzqq/hare/datastores/net"
+	"github.com/ohzqq/hare/datastores/ram"
+	"github.com/ohzqq/hare/datastores/store"
+)
+
+type Data interface {
+	New(string) (Data, error)
+	Open(string) (Data, error)
+	hare.Datastorage
+}
+
+type Opt func(*DB) error
+
+func NewDisk(path string) Opt {
+	return func(db *DB) error {
+		ds, err := NewDiskStorage(path)
+		if err != nil {
+			return err
+		}
+		return db.Init(ds)
+	}
+}
+
+func WithDisk(path string) Opt {
+	return func(db *DB) error {
+		ds, err := OpenDisk(path)
+		if err != nil {
+			return err
+		}
+		return db.Init(ds)
+	}
+}
+
+func WithURL(uri string, d []byte) Opt {
+	return func(db *DB) error {
+		ds, err := NewNet(uri, d)
+		if err != nil {
+			return err
+		}
+		return db.Init(ds)
+	}
+}
+
+func WithData(d []byte) Opt {
+	return func(db *DB) error {
+		m := map[string][]byte{
+			"index": d,
+		}
+		ds, err := ram.New(m)
+		if err != nil {
+			return err
+		}
+		return db.Init(ds)
+	}
+}
+
+func NewDiskStore(path string) (hare.Datastorage, error) {
+	return NewDiskStorage(path)
+}
+
+func NewRamStore(path string) (hare.Datastorage, error) {
+	return NewMem()
+}
+
+func NewNetStore(path string, d []byte) (hare.Datastorage, error) {
+	return NewNet(path, d)
+}
+
+func OpenDisk(path string) (*disk.Disk, error) {
+	ds, err := disk.New(path, ".json")
+	if err != nil {
+		return nil, err
+	}
+	return ds, nil
+}
+
+func NewDiskStorage(path string) (*disk.Disk, error) {
+	ds, err := disk.New(path, ".json")
+	if err != nil {
+		return nil, err
+	}
+	if !ds.TableExists("index") {
+		err = ds.CreateTable("index")
+		if err != nil {
+			return nil, err
+		}
+	}
+	if !ds.TableExists("index-settings") {
+		err = ds.CreateTable("index-settings")
+		if err != nil {
+			return nil, err
+		}
+	}
+	return ds, nil
+}
+
+func NewMem() (*ram.Ram, error) {
+	r := &ram.Ram{
+		Store: store.New(),
+	}
+	err := r.CreateTable("index")
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.CreateTable("index-settings")
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func NewNet(uri string, d []byte) (*net.Net, error) {
+	ds, err := net.New(uri, d)
+	if err != nil {
+		return nil, err
+	}
+	return ds, nil
+}
